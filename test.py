9f908f8754c6c2edaa1bd78b8cd281eb96024cf4
ramonborges <ramonborges@gmail.com>
2023-06-21 13:24:26 -0400

    adicionando badge para o Pipeline

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Ramon Borges")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps")
 
 
 if __name__ == "__main__":
